474ae175a52ecdd7c6a917b5ed300dc4b07a7869
1234abhibisht <abhibisht129@gmail.com>
2026-07-02 20:25:15 +0530

    Update 13_CopyingAndInsertingAVectorToAnother.cpp

--- 02_STL/02_Vector/13_CopyingAndInsertingAVectorToAnother.cpp
+++ 02_STL/02_Vector/13_CopyingAndInsertingAVectorToAnother.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4};
-    vector<int> copy(2, 10); /* copies {10,10} */
-    v.insert(v.begin() + 1, copy.begin(), copy.end());
+    vector<int> temp(2, 10); /* copies {10,10} */
+    v.insert(v.begin(), temp.begin(), temp.end());
     for (auto it : v)
     {
         cout << it << " ";
