fd1869aa03ab3476727053b4ab22363be67fbb74
1234abhibisht <abhibisht129@gmail.com>
2026-08-15 11:26:31 +0530

    Update 02_BetterMethod.cpp

--- 05_DSAQuestionsUsingAllMethods/01_Array(Easy)/14_LongestSubArrayWithSumK/02_BetterMethod.cpp
+++ 05_DSAQuestionsUsingAllMethods/01_Array(Easy)/14_LongestSubArrayWithSumK/02_BetterMethod.cpp
@@ -6,24 +6,20 @@ using namespace std;
 int longestSubArray(vector<int> &arr, int n, long long k)
 {
     map<long long, int> mpp;
+    mpp[0] = -1;
     int sum = 0, len = 0;
     for (int i = 0; i <= n - 1; i++)
     {
         sum = sum + arr[i];
-        if (sum == k)
-        {
-            len = max(len, i + 1);
-        }
         int rem = sum - k;
         // first check whether rem key is present in map, if not then put in map
-
         if (mpp.find(rem) != mpp.end())
         { // find(rem) is not pointing to address next to where map ends,
             // means find(rem) is pointing to address inside the map, means key exists in map
             len = max(len, i - mpp[rem]);
         }
         if (mpp.find(sum) == mpp.end())
-        {
+        {   // for handling zeroes
             mpp[sum] = i;
         }
     }
@@ -31,7 +27,7 @@ int longestSubArray(vector<int> &arr, int n, long long k)
 }
 int main()
 {
-    vector<int> arr = {2, 0, 0, 3};
+    vector<int> arr = {1, 2, 3, 1, 2};
     int size = arr.size();
     cout << longestSubArray(arr, size, 3);
     return 0;
